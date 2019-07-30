0c34b4c6524dee915eff6da276566e53bb9460b4
Audie Bethea <audiedane@gmail.com>
2019-07-30 16:29:09 -0500

    updated onSumbit in questionnaire to delete errorMessage on success

--- client/src/components/questionnaire/questionnaire.js
+++ client/src/components/questionnaire/questionnaire.js
@@ -71,6 +71,7 @@ export default class Questionnaire extends React.Component{
             this.setState({errorMessage : "You must select at least one interest."});
         }
         else{
+            this.setState({errorMessage : ""});
             this.props.gotoPage("Results");
         }
     }
