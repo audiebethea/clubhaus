ea6ce6444219718f4643d03307e2f7fcc95233c7
Audie Bethea <audiedane@gmail.com>
2019-08-10 12:13:36 -0500

    did some basic resultcard css

--- client/src/components/higherordercomponents/resultcard.js
+++ client/src/components/higherordercomponents/resultcard.js
@@ -4,6 +4,8 @@ import React from 'react';
 import {withStyles} from '@material-ui/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import '../results/results.css';
 
 
 const styles = {
@@ -16,21 +18,25 @@ const styles = {
 }
 
 function ResultCard(props){
-    const {classes} = props;
 
     return (
-        <Card raised = {true} classes={{root : classes.cardRoot}}>
-            <h2 style={{float:'left', marginTop:'4%', marginLeft:'25%', fontSize:'calc(5px + 1.4vw'}}>{props.club.name}</h2>
-            <img src={props.club.logoLink} alt="" style={{height:'7%', width:'7%', float:'right', marginTop:'2%', marginRight:'25%'}}></img>
-            <hr style={{color : 'rgb(102, 101, 101)', margin : '12% 10% 2%', border: '1px solid'}}></hr>
+        <Card raised = {true} style={{margin : '0 20%'}}>
+            <h2 className='card-title'>{props.club.name}</h2>
+            <img src={props.club.logoLink} alt="" className='club-logo'></img>
+            <hr className='underline-card'></hr>
             <CardContent>
-                <p align = "start">
-                    Match % : {props.club.matchPercent}
-                </p>
-                <p align = "start">
-                    Matched Interests : {props.club.matchedInterests.toString()}
-                </p>
-                <a href={props.club.clubLink} target='_blank' rel="noopener noreferrer" align="end">More Info About This Club</a>
+            <p className='description'>
+                Description : {props.club.description}
+            </p>
+            <p className="match-percent">
+                Match % : {props.club.matchPercent}
+            </p>
+            <p className="matched-interests">
+                Matched Interests : {props.club.matchedInterests.toString()}
+            </p> 
+            <a href={props.club.clubLink} target='_blank' rel="noopener noreferrer" className='learn-more'>
+                <Button>Learn More About This Club!</Button>
+            </a>
             </CardContent>
         </Card>
     )
